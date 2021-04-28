cc2c54c5079d18929a2e80a1e72a504bb1710ea0
Todd Murphy <tmurphy605@icloud.com>
2021-04-28 14:12:10 -0400

    added valid url package

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { isUri } from 'valid-url';
 
 (async () => {
 
